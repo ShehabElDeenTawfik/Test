15a57437c8c0315f75b2353f98c074d636d2f06e
Shehab El Deen <shohob2009@gmail.com>
2025-04-29 00:54:44 +0300

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+
+     ChromeDriver driver = new ChromeDriver();
+
+     String url ="https://the-internet.herokuapp.com/login";
+        /* open url */
+     driver.get(url);
+     //enter username
+     driver.findElement(By.tagName("input")).sendKeys("tomsmith");
+     //enter pass
+     driver.findElement(By.name("password")).sendKeys("SuperSecretPassword!");
+     //click login button
+    driver.findElement(By.className("fa-2x")).click();
+
+
+
+
+
+    }
+}
